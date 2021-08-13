cb08d146907db87c3cfe12c68b9b642c1ede997f
Renato Alexandre <renatoacf@gmail.com>
2021-08-13 19:18:11 -0300

    first commit

---
+++ src/test/java/br/com/aracatidigital/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aracatidigital.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
